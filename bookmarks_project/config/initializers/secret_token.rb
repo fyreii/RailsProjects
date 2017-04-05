ba7fad1742d199187168ac1ef31e18c66c9d4c94
Teresa Henderson <thenderson@webmd.net>
2017-04-05 10:44:11 -0700

    First commit

---
+++ bookmarks_project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookmarksProject::Application.config.secret_key_base = 'b59d5d3feaf614879dd408013f51a2c9a17ee37d2bf650a5a7a11cbec32495ab334b986e8b21d7f95cd59d99eb61eccb20c8a0200f35b20f211fc37bb2823020'
---
+++ bookmarks_project/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookmarksProject::Application.config.session_store :cookie_store, key: '_bookmarks_project_session'
