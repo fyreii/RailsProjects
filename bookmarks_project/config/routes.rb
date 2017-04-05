de8c3d72c257afd795daee4beac55e73748e6aa5
Teresa Henderson <thenderson@webmd.net>
2017-04-05 12:20:21 -0700

    Second commit halfway through build process

--- bookmarks_project/config/routes.rb
+++ bookmarks_project/config/routes.rb
@@ -1,4 +1,7 @@
 BookmarksProject::Application.routes.draw do
+  devise_for :users
+  resources :bookmarks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
